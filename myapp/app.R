c453fefedc30f91a488337684b0488a2c026b7e6
Dr. Janine Buchholz <buchjani@user.hu-berlin.de>
2023-10-26 16:43:04 +0200

    check if updates take action immediately

--- myapp/app.R
+++ myapp/app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Deploying my Shiny app on Github Pages"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
